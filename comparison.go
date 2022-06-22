bd66eab6f7d9c820467bf2522fbf1ee344792876
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-22 17:04:48 +0530

    Add helper function to check for equality

---
+++ comparison.go
@@ -0,0 +1,18 @@
+package sliceutils
+
+import "golang.org/x/exp/constraints"
+
+//	Check if two slices are equal
+func Equal[T constraints.Ordered](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, value := range a {
+		if value != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
