5f7da5eb683d6bdff38861e7524fa0581a1cba02
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-22 23:56:44 +0530

    Add basic tests

--- higher_order_functions_test.go
+++ higher_order_functions_test.go
@@ -1,16 +1,48 @@
 package sliceutils
 
 import (
-	"fmt"
 	"testing"
 )
 
+var slice []int = []int{0, 1, 2, 3, 4, 5, 6}
+
 func TestForEach(t *testing.T) {
 
-	slice := []string{"A", "B", "C"}
+	length := 0
+	ForEach(slice, func(_, _ int) { length++ })
 
-	ForEach(slice, func(char string, idx int) {
-		fmt.Println(idx, char)
-	})
+	if length != len(slice) {
+		t.Error("Failed to forEach over a slice")
+	}
+
+}
+
+func TestFilter(t *testing.T) {
+
+	filteredSlice := Filter(slice, func(v, _ int) bool { return v%2 == 0 })
+
+	if !Equal(filteredSlice, []int{0, 2, 4, 6}) {
+		t.Error("Failed to filter slice")
+	}
+
+}
+
+func TestMap(t *testing.T) {
+
+	mappedSlice := Map(slice, func(v, _ int) int { return v * 2 })
+
+	if !Equal(mappedSlice, []int{0, 2, 4, 6, 8, 10, 12}) {
+		t.Error("Failed to map over slice")
+	}
+
+}
+
+func TestReduce(t *testing.T) {
+
+	sum := Reduce(slice, func(accumulator, current int, _ int, _ []int) int { return accumulator + current }, 0)
+
+	if sum != 21 {
+		t.Error("Failed to reduce slice")
+	}
 
 }
