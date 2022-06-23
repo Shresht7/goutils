da9e9db3739957e10ee4f6cd2d64541ab7da22bf
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-23 14:32:05 +0530

    Implement Set Methods

--- set_test.go
+++ set_test.go
@@ -2,14 +2,28 @@ package sliceutils
 
 import "testing"
 
-func TestGetSet(t *testing.T) {
+func TestSet(t *testing.T) {
 
-	slice := []int{0, 1, 1, 2, 3, 3, 3, 4, 5}
+	slice := []int{0, 1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 5, 5}
 
 	set := GetSet(slice)
 
 	if !Equal(set, []int{0, 1, 2, 3, 4, 5}) {
 		t.Error("Failed to create set")
+	}
+
+	set.Add(6)
+	set.Add(7)
+	set.Add(7)
+
+	if set.Size() != 8 {
+		t.Error("Failed to add to set")
+	}
+
+	set.Clear()
+
+	if set.Size() != 0 {
+		t.Error("Failed to clear set")
 	}
 
 }
