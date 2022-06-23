af3dfef6294a79b265cfddc1b5fa12a7ff40e87e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-23 14:32:05 +0530

    Implement Set Methods

--- set.go
+++ set.go
@@ -2,7 +2,7 @@ package sliceutils
 
 import "golang.org/x/exp/constraints"
 
-type Set[T any] []T
+type Set[T constraints.Ordered] []T
 
 func GetSet[T constraints.Ordered](slice []T) Set[T] {
 	result := make([]T, 0, len(slice))
@@ -15,4 +15,32 @@ func GetSet[T constraints.Ordered](slice []T) Set[T] {
 		}
 	}
 	return result
+}
+
+//	Returns the size of the set
+func (s *Set[T]) Size() int {
+	return len(*s)
+}
+
+//	Set has value
+func (s *Set[T]) Has(value T) bool {
+	return Includes(*s, value)
+}
+
+//	Add another element to the set
+func (s *Set[T]) Add(value T) *Set[T] {
+	if !s.Has(value) {
+		*s = append(*s, value)
+	}
+	return s
+}
+
+//	TODO: Needs Splice
+// func (s *Set[T]) Delete(value T) *Set[T] {
+
+// }
+
+//	Clears the entire set
+func (s *Set[T]) Clear() {
+	*s = make([]T, 0, s.Size())
 }
