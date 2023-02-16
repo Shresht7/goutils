94e6722ec49a1c237f94b67bcfb7a027fb9876b2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-16 19:46:52 +0530

    Refactor `higher_order_functions`

--- slice/higher_order_functions.go
+++ slice/higher_order_functions.go
@@ -1,15 +1,25 @@
 package slice
 
-//	Execute the callback function for each element of the slice.
-//	The callback receives the element's value and index.
+// * FOREACH *
+
+// Execute the callback function for each element of the slice.
+// The callback receives the element's value and index.
 func ForEach[T any](slice []T, cb Callback[T]) {
 	for i, v := range slice {
 		cb(v, i)
 	}
 }
 
-//	Filter the slice based on the callback criteria. Elements are filtered out
-//	if the callback returns false. The callback receives the element's value and index.
+// Execute the callback function for each element of the slice.
+// The callback receives the element's value and index.
+func (slice *Slice[T]) ForEach(cb Callback[T]) {
+	ForEach(*slice, cb)
+}
+
+// * FILTER *
+
+// Filter the slice based on the callback criteria. Elements are filtered out
+// if the callback returns false. The callback receives the element's value and index.
 func Filter[T any](slice []T, cb ReturnCallback[T, bool]) []T {
 	ret := make([]T, 0, len(slice))
 
@@ -22,8 +32,16 @@ func Filter[T any](slice []T, cb ReturnCallback[T, bool]) []T {
 	return ret
 }
 
-//	Map returns a new slice by applying the callback to each element of the given slice.
-//	The callback receives the value and the index and returns the mapped value.
+// Filter the slice based on the callback criteria. Elements are filtered out
+// if the callback returns false. The callback receives the element's value and index.
+func (slice *Slice[T]) Filter(cb ReturnCallback[T, bool]) []T {
+	return Filter(*slice, cb)
+}
+
+// * MAP *
+
+// Map returns a new slice by applying the callback to each element of the given slice.
+// The callback receives the value and the index and returns the mapped value.
 func Map[From, To any](slice []From, cb ReturnCallback[From, To]) []To {
 	ret := make([]To, len(slice))
 
@@ -34,8 +52,16 @@ func Map[From, To any](slice []From, cb ReturnCallback[From, To]) []To {
 	return ret
 }
 
-//	Reduces a slice into a single value using the given callback function.
-//	The callback receives the accumulated value, current value, index and the slice.
+// Map returns a new slice by applying the callback to each element of the given slice.
+// The callback receives the value and the index and returns the mapped value.
+func (slice *Slice[T]) Map(cb ReturnCallback[T, T]) []T {
+	return Map(*slice, cb)
+}
+
+// * REDUCE *
+
+// Reduces a slice into a single value using the given callback function.
+// The callback receives the accumulated value, current value, index and the slice.
 func Reduce[From, To any](slice []From, cb ReducerCallback[From, To], initializer To) To {
 	val := initializer
 
@@ -46,7 +72,15 @@ func Reduce[From, To any](slice []From, cb ReducerCallback[From, To], initialize
 	return val
 }
 
-//	Returns true if all elements satisfy the callback function
+// Reduces a slice into a single value using the given callback function.
+// The callback receives the accumulated value, current value, index and the slice.
+func (slice *Slice[T]) Reduce(cb ReducerCallback[T, T], initializer T) T {
+	return Reduce(*slice, cb, initializer)
+}
+
+// * EVERY *
+
+// Returns true if all elements satisfy the callback function
 func Every[T any](slice []T, cb ReturnCallback[T, bool]) bool {
 
 	for i, v := range slice {
@@ -59,7 +93,14 @@ func Every[T any](slice []T, cb ReturnCallback[T, bool]) bool {
 	return true
 }
 
-//	Returns true if any one element satisfies the callback function
+// Returns true if all elements satisfy the callback function
+func (slice *Slice[T]) Every(cb ReturnCallback[T, bool]) bool {
+	return Every(*slice, cb)
+}
+
+// * SOME *
+
+// Returns true if any one element satisfies the callback function
 func Some[T any](slice []T, cb ReturnCallback[T, bool]) bool {
 
 	for i, v := range slice {
@@ -71,11 +112,18 @@ func Some[T any](slice []T, cb ReturnCallback[T, bool]) bool {
 	return false
 }
 
+// Returns true if any one element satisfies the callback function
+func (slice *Slice[T]) Some(cb ReturnCallback[T, bool]) bool {
+	return Some(*slice, cb)
+}
+
 //	? Use All and Any instead of Every and Some
 
-//	Returns the first element in the slice that satisfies the callback criteria.
-//	Returns a boolean indicating the success of the find operation, followed by the value and it's index.
-//	If the criteria fails, the boolean is false - i.e. the function failed to find the value.
+// * FIND *
+
+// Returns the first element in the slice that satisfies the callback criteria.
+// Returns a boolean indicating the success of the find operation, followed by the value and it's index.
+// If the criteria fails, the boolean is false - i.e. the function failed to find the value.
 func Find[T any](slice []T, cb ReturnCallback[T, bool]) (bool, T, int) {
 
 	for i, v := range slice {
@@ -88,7 +136,16 @@ func Find[T any](slice []T, cb ReturnCallback[T, bool]) (bool, T, int) {
 
 }
 
-//	Returns true when the slice contains the given element.
+// Returns the first element in the slice that satisfies the callback criteria.
+// Returns a boolean indicating the success of the find operation, followed by the value and it's index.
+// If the criteria fails, the boolean is false - i.e. the function failed to find the value.
+func (slice *Slice[T]) Find(cb ReturnCallback[T, bool]) (bool, T, int) {
+	return Find(*slice, cb)
+}
+
+// * INCLUDES *
+
+// Returns true when the slice contains the given element.
 func Includes[T comparable](slice []T, element T) bool {
 	ok, _, _ := Find(slice, func(v T, _ int) bool { return v == element })
 	return ok
--- slice/slice.go
+++ slice/slice.go
@@ -23,51 +23,6 @@ func (slice *Slice[T]) AsSlice() []T {
 	return *slice
 }
 
-//	Executes the callback function for each element of the slice.
-//	The callback receives the value and the index of the element.
-func (slice *Slice[T]) ForEach(cb Callback[T]) {
-	ForEach(*slice, cb)
-}
-
-//	Filter the slice based on the callback criteria. Elements are filtered out
-//	if the callback returns false. The callback receives the element's value and index.
-func (slice *Slice[T]) Filter(cb ReturnCallback[T, bool]) []T {
-	return Filter(*slice, cb)
-}
-
-//	Map returns a new slice by applying the callback to each element of the given slice.
-//	The callback receives the value and the index and returns the mapped value.
-//	Note that the Map method can only return a slice of the same type.
-//	For more powerful transforms use the Map function.
-func (slice *Slice[T]) Map(cb ReturnCallback[T, T]) []T {
-	return Map(*slice, cb)
-}
-
-//	Reduces a slice into a single value using the given callback function.
-//	The callback receives the accumulated value, current value, index and the slice.
-//	Note that the Reduce method can only reduce the slice to a value of the constituting type.
-//	For more powerful transforms use the Reduce function.
-func (slice *Slice[T]) Reduce(cb ReducerCallback[T, T], initializer T) T {
-	return Reduce(*slice, cb, initializer)
-}
-
-//	Returns true if all elements satisfy the callback function
-func (slice *Slice[T]) Every(cb ReturnCallback[T, bool]) bool {
-	return Every(*slice, cb)
-}
-
-//	Returns true if any one element satisfies the callback function
-func (slice *Slice[T]) Some(cb ReturnCallback[T, bool]) bool {
-	return Some(*slice, cb)
-}
-
-//	Returns the first element in the slice that satisfies the callback criteria.
-//	Returns a boolean indicating the success of the find operation, followed by the value and it's index.
-//	If the criteria fails, the boolean is false - i.e. the function failed to find the value.
-func (slice *Slice[T]) Find(cb ReturnCallback[T, bool]) (bool, T, int) {
-	return Find(*slice, cb)
-}
-
 //	Append slices to this slice
 func (slice *Slice[T]) Concat(slices ...[]T) Slice[T] {
 	for _, v := range slices {
