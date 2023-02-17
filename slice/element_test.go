0babc48319714f7b2a0a0a485fa32dee3109ee6b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-17 11:26:00 +0530

    Implement the `At` function

--- slice/element_test.go
+++ slice/element_test.go
@@ -90,3 +90,70 @@ func ExampleNth() {
 	// Output:
 	// 2
 }
+
+// * AT *//
+
+func TestAt(t *testing.T) {
+
+	// Test Data
+	slice := []int{0, 1, 2, 3}
+
+	// Test Cases
+	testCases := []struct {
+		desc     string
+		actual   int
+		expected int
+	}{
+		{
+			desc:     "Should return the first element of the slice",
+			actual:   At(slice, 0),
+			expected: 0,
+		},
+		{
+			desc:     "Should return the last element of the slice",
+			actual:   At(slice, -1),
+			expected: 3,
+		},
+		{
+			desc:     "Should return the nth element of the slice",
+			actual:   At(slice, 2),
+			expected: 2,
+		},
+	}
+
+	// Run Tests
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if tC.actual != tC.expected {
+				t.Errorf("%s\nwant %d, got %d", tC.desc, tC.expected, tC.actual)
+			}
+		})
+	}
+
+}
+
+func ExampleAt() {
+	a := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(At(a, 0))
+	fmt.Println(At(a, -1))
+	fmt.Println(At(a, 2))
+
+	// Output:
+	// 1
+	// 5
+	// 3
+}
+
+func ExampleAt_method() {
+	a := New([]int{1, 2, 3, 4, 5})
+
+	fmt.Println(a.At(0))
+	fmt.Println(a.At(-1))
+	fmt.Println(a.At(2))
+
+	// Output:
+	// 1
+	// 5
+	// 3
+}
