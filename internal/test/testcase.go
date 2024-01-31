cebc298ecc6c62ed8ab7587a9b551c4ce32aafaf
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-31 09:05:05 +0530

    Update comments

--- internal/test/testcase.go
+++ internal/test/testcase.go
@@ -4,23 +4,19 @@ package test
 // TEST CASE
 // ---------
 
-// TestCase is a struct that contains the information needed to describe a test case.
+// TestCase is a struct that contains the information needed to describe a test case
 type TestCase[T any] struct {
-	// Description of the test case
-	Desc string
-	// Function to generate the actual value
-	Fn func() T
-	// Expected value to compare the actual value with
-	Expected T
-	// Function to compare the actual and expected values. If true, the test case has failed.
-	Fail func(actual, expected T) bool
+	Desc     string                        // Description of the test case
+	Fn       func() T                      // Function to generate the actual value
+	Expected T                             // Expected value to compare the actual value with
+	Fail     func(actual, expected T) bool // Function to compare the actual and expected values. If true, the test case has failed
 }
 
 // -------
 // HELPERS
 // -------
 
-// Inequality is a helper function that returns true if the actual value is not equal to the expected value.
+// Inequality is a helper function that returns true if the actual value is not equal to the expected value
 func Inequality[T comparable](actual, expected T) bool {
 	return actual != expected
 }
