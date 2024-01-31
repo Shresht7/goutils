f77f1d188eb89f7df37370f9034809790c097b07
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-01-31 12:49:18 +0530

    Create `Uncapitalize` function

--- strings/case.go
+++ strings/case.go
@@ -9,3 +9,12 @@ func Capitalize(s string) string {
 	}
 	return strings.ToUpper(s[:1]) + s[1:]
 }
+
+// Uncapitalize the string (make the first letter lowercase)
+func Uncapitalize(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToLower(s[:1]) + s[1:]
+}
+
--- strings/case_test.go
+++ strings/case_test.go
@@ -30,3 +30,34 @@ func TestCapitalize(t *testing.T) {
 	}
 
 }
+
+func TestUncapitalize(t *testing.T) {
+
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"", ""},
+		{"a", "a"},
+		{"A", "a"},
+		{"abc", "abc"},
+		{"Abc", "abc"},
+		{"ABC", "aBC"},
+		{"123", "123"},
+		{"123abc", "123abc"},
+		{"123Abc", "123Abc"},
+		{"123ABC", "123ABC"},
+		{"abc123", "abc123"},
+		{"Abc123", "abc123"},
+		{"A sentence", "a sentence"},
+		{"A Sentence", "a Sentence"},
+	}
+
+	for _, testCase := range testCases {
+		actual := Uncapitalize(testCase.input)
+		if actual != testCase.expected {
+			t.Errorf("Uncapitalize(%q) expected %q, got %q", testCase.input, testCase.expected, actual)
+		}
+	}
+
+}
